c3668dee52bd1427cabf2d5e2830041a0c098ed6
Witek <witus2706@gmail.com>
2020-02-18 09:26:18 +0100

    some further implementation added

--- src/main/scala/main/chapter7/ParallelComputation.scala
+++ src/main/scala/main/chapter7/ParallelComputation.scala
@@ -2,7 +2,9 @@ package main.chapter7
 
 import java.util.concurrent.ExecutorService
 
-import scala.concurrent.Future
+import scala.concurrent.duration.{Duration, TimeUnit}
+import scala.concurrent.{CanAwait, ExecutionContext, Future}
+import scala.util.Try
 
 trait ParallelComputation {
   type Par[A] = ExecutorService => Future[A]
@@ -16,4 +18,47 @@ trait ParallelComputation {
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
   def run[A](a: Par[A]): A
+}
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) =>
+
+  )
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.value.get.get, bf.value.get.get))
+    }
+
+  def asyncF[A, B](f: A => B): A => Par[B]
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[A]] = {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    null
+  }
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit) = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+
+    override def onComplete[U](f: Try[A] => U)(implicit executor: ExecutionContext): Unit = ???
+
+    override def isCompleted: Boolean = true
+
+    override def value: Option[Try[A]] = ???
+
+    override def ready(atMost: Duration)(implicit permit: CanAwait): UnitFuture.this.type = ???
+
+    override def result(atMost: Duration)(implicit permit: CanAwait): A = ???
+  }
+
 }
